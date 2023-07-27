aa016ed50b3e98fe470e688d47945dbe4eb9ce69
atul2602 <atuliraz1305@gmail.com>
2023-07-27 17:15:35 +0530

    Updated PyTorch code, some errors pending

--- timeganPytorch.py
+++ timeganPytorch.py
@@ -62,11 +62,17 @@ def timegan(ori_data, parameters):
             self.fc = nn.Linear(hidden_dim, hidden_dim)
 
         def forward(self, x, t):
-            #packed_x = nn.utils.rnn.pack_padded_sequence(x, t, batch_first=True, enforce_sorted=False)
-            output, h_n = self.rnn(x)
-            H = self.fc(output)  # Extract the output of the last layer
+            sorted_lengths, sorted_idx = torch.sort(t, descending=True)
+            sorted_x = x[sorted_idx]
+            packed_x = nn.utils.rnn.pack_padded_sequence(sorted_x, sorted_lengths, batch_first=True, enforce_sorted=False)
+            output, _ = self.rnn(packed_x)
+            padded_output, _ = nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
+            _, unsorted_idx = torch.sort(sorted_idx)
+            H = padded_output[unsorted_idx]
+            H = self.fc(H)
             return H
-    # Recovery Network
+
+
     class Recovery(nn.Module):
         def __init__(self, hidden_dim, num_layers, dim):
             super(Recovery, self).__init__()
@@ -74,12 +80,16 @@ def timegan(ori_data, parameters):
             self.fc = nn.Linear(hidden_dim, dim)
 
         def forward(self, h, t):
-            #packed_h = nn.utils.rnn.pack_padded_sequence(h, t, batch_first=True, enforce_sorted=False)
-            r_outputs, _ = self.rnn(h)
-            X_tilde = self.fc(r_outputs)  # Extract the output of the last layer
+            sorted_lengths, sorted_idx = torch.sort(t, descending=True)
+            sorted_h = h[sorted_idx]
+            packed_h = nn.utils.rnn.pack_padded_sequence(sorted_h, sorted_lengths, batch_first=True, enforce_sorted=False)
+            output, _ = self.rnn(packed_h)
+            padded_output, _ = nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
+            _, unsorted_idx = torch.sort(sorted_idx)
+            X_tilde = self.fc(padded_output[unsorted_idx])
             return X_tilde
 
-    # Generator Network
+
     class Generator(nn.Module):
         def __init__(self, hidden_dim, num_layers, dim):
             super(Generator, self).__init__()
@@ -87,26 +97,38 @@ def timegan(ori_data, parameters):
             self.fc = nn.Linear(hidden_dim, hidden_dim)
 
         def forward(self, z, t):
-            z = z.to(self.fc.weight.dtype) 
-            #packed_z = nn.utils.rnn.pack_padded_sequence(z, t, batch_first=True, enforce_sorted=False)
-            output, g_last_states = self.rnn(z)
-            E = self.fc(output)  # Extract the output of the last layer
-            return E
-
-    # Supervisor Network
+            print("Data types:")
+            print("z dtype:", z.dtype)
+            print("t dtype:", t.dtype)
+            sorted_lengths, sorted_idx = torch.sort(t, descending=True)
+            sorted_z = z[sorted_idx]
+            packed_z = nn.utils.rnn.pack_padded_sequence(sorted_z, sorted_lengths, batch_first=True, enforce_sorted=False)
+            output, _ = self.rnn(packed_z)
+            padded_output, _ = nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
+            _, unsorted_idx = torch.sort(sorted_idx)
+            G = self.fc(padded_output[unsorted_idx])
+            print("G shape is: ", G.shape)
+            return G
+        
     class Supervisor(nn.Module):
         def __init__(self, hidden_dim, num_layers, dim):
             super(Supervisor, self).__init__()
-            self.rnn = nn.RNN(hidden_dim, hidden_dim, num_layers=num_layers-1, batch_first=True)
+            self.rnn = nn.RNN(hidden_dim, hidden_dim, num_layers=num_layers, batch_first=True)
             self.fc = nn.Linear(hidden_dim, hidden_dim)
 
         def forward(self, h, t):
-            #packed_h = nn.utils.rnn.pack_padded_sequence(h, t, batch_first=True, enforce_sorted=False)
-            output, s_last_states = self.rnn(h)
-            S = self.fc(output)  # Extract the output of the last layer
-            return S
+            print("Hidden dimension is: ", h.shape)
+            sorted_lengths, sorted_idx = torch.sort(t, descending=True)
+            sorted_h = h[sorted_idx]
+            packed_h = nn.utils.rnn.pack_padded_sequence(sorted_h, sorted_lengths, batch_first=True, enforce_sorted=False)
 
-    # Discriminator Network
+            output, _ = self.rnn(packed_h)
+            padded_output, _ = nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
+            _, unsorted_idx = torch.sort(sorted_idx)
+            S = padded_output[unsorted_idx]
+            S = self.fc(S)
+            return S
+    
     class Discriminator(nn.Module):
         def __init__(self, hidden_dim, num_layers):
             super(Discriminator, self).__init__()
@@ -114,10 +136,15 @@ def timegan(ori_data, parameters):
             self.fc = nn.Linear(hidden_dim, 1)
 
         def forward(self, h, t):
-            #packed_h = nn.utils.rnn.pack_padded_sequence(h, t, batch_first=True, enforce_sorted=False)
-            output, d_last_states = self.rnn(h)
-            Y_hat = self.fc(output)  # Extract the output of the last layer
+            sorted_lengths, sorted_idx = torch.sort(t, descending=True)
+            sorted_h = h[sorted_idx]
+            packed_h = nn.utils.rnn.pack_padded_sequence(sorted_h, sorted_lengths, batch_first=True, enforce_sorted=False)
+            output, _ = self.rnn(packed_h)
+            padded_output, _ = nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
+            _, unsorted_idx = torch.sort(sorted_idx)
+            Y_hat = self.fc(padded_output[unsorted_idx])
             return Y_hat
+
 
     # Convert Numpy arrays to PyTorch tensors
     ori_data = torch.FloatTensor(ori_data)
@@ -142,31 +169,35 @@ def timegan(ori_data, parameters):
     # Training
     print('Start Embedding Network Training')
     for itt in range(iterations):
-        embedder_optimizer.zero_grad()
 
         # Set mini-batch
         X_mb, T_mb = batch_generator(ori_data, ori_time, batch_size)
-        X_mb = torch.stack([torch.tensor(x) for x in X_mb])
-        T_mb = torch.stack([torch.tensor(t) for t in T_mb])
+        X_mb = torch.stack([torch.tensor(x) for x in X_mb])  # Convert X_mb to PyTorch tensor
+        T_mb = torch.stack([torch.tensor(t) for t in T_mb]) 
 
-        # Embedder training
+        # Zero the gradients
+        embedder_optimizer.zero_grad()
+
+        # Embedder forward pass
         H = embedder(X_mb, T_mb)
 
-        H = torch.tensor(H)
-
+        # Calculate the reconstruction loss
         X_tilde = recovery(H, T_mb)
-        E_loss_T0 = mse_loss(X_mb, X_tilde)
+        E_loss_T0 = nn.MSELoss()(X_mb, X_tilde)
         E_loss0 = torch.sqrt(E_loss_T0)
-        E_loss = E_loss0 + 0.1 * torch.sqrt(mse_loss(H[:, 1:, :], supervisor(H[:, :-1, :], T_mb)))
+        E_loss = E_loss0 + 0.1 * torch.sqrt(nn.MSELoss()(H[:, 1:, :], H[:, :-1, :]))
 
+        # Backpropagation and optimization
         E_loss.backward()
         embedder_optimizer.step()
 
         # Checkpoint
-        if itt % 10 == 0:
-            print('step: ' + str(itt) + '/' + str(iterations) + ', e_loss: ' + str(np.round(E_loss0.item(), 4)))
-
+        if itt % 1000 == 0:
+            print('step: ' + str(itt) + '/' + str(iterations) + ', e_loss: ' + str(torch.sqrt(E_loss_T0).item()))
     print('Finish Embedding Network Training')
+
+    with torch.no_grad():
+        H_supervise = embedder(X_mb, T_mb)  # Generate H for supervised loss later
 
     print('Start Training with Supervised Loss Only')
     for itt in range(iterations):
@@ -178,17 +209,26 @@ def timegan(ori_data, parameters):
         # Random vector generation
         Z_mb = random_generator(batch_size, z_dim, T_mb, max_seq_len)
 
-        X_mb = torch.stack([torch.tensor(x) for x in X_mb])
-        T_mb = torch.stack([torch.tensor(t) for t in T_mb])
-        Z_mb = torch.stack([torch.tensor(z) for z in Z_mb])
+        X_mb = torch.stack([torch.tensor(x, dtype = torch.float32) for x in X_mb])
+        T_mb = torch.stack([torch.tensor(t, dtype = torch.int64) for t in T_mb])
+        Z_mb = torch.stack([torch.tensor(z, dtype = torch.float32) for z in Z_mb])
+        print("X_mb shape is: ", X_mb.shape)
+        print("T_mb shape is: ", T_mb.shape)
+        print("Z_mb shape is: ", Z_mb.shape)
 
-        # Generator training
+
         E_hat = generator(Z_mb, T_mb)
         H_hat = supervisor(E_hat, T_mb)
+        G_loss_S = torch.sqrt(mse_loss(H_hat[:, 1:, :], H_hat[:, :-1, :]))
 
-        G_loss_S = torch.sqrt(mse_loss(H[:, 1:, :], H_hat[:, :-1, :]))
-        G_loss_S.backward()
+        # Calculate the supervised loss using the stored H
+        G_loss_S_supervised = torch.sqrt(mse_loss(H_hat[:, 1:, :], H_supervise[:, :-1, :]))
 
+        # Calculate the total G_loss_S using both supervised and unsupervised components
+        G_loss_S_total = G_loss_S + G_loss_S_supervised
+
+        # Backpropagate with the combined loss
+        G_loss_S_total.backward()
         generator_optimizer.step()
 
         # Checkpoint
@@ -197,10 +237,14 @@ def timegan(ori_data, parameters):
 
     print('Finish Training with Supervised Loss Only')
 
+    with torch.no_grad():
+        H_supervise = embedder(X_mb, T_mb)  # Generate H for supervised loss later
+
     print('Start Joint Training')
     for itt in range(iterations):
         # Generator training (twice more than discriminator training)
         for kk in range(2):
+            # confused about the H_supervise in this part.
             generator_optimizer.zero_grad()
 
             # Set mini-batch
@@ -209,19 +253,19 @@ def timegan(ori_data, parameters):
             # Random vector generation
             Z_mb = random_generator(batch_size, z_dim, T_mb, max_seq_len)
 
-            X_mb = torch.stack([torch.tensor(x) for x in X_mb])
-            T_mb = torch.stack([torch.tensor(t) for t in T_mb])
-            Z_mb = torch.stack([torch.tensor(z) for z in Z_mb])
+            X_mb = torch.stack([torch.tensor(x, dtype = torch.float32) for x in X_mb])
+            T_mb = torch.stack([torch.tensor(t, dtype = torch.int64) for t in T_mb])
+            Z_mb = torch.stack([torch.tensor(z, dtype = torch.float32) for z in Z_mb])
 
             # Generator training
             E_hat = generator(Z_mb, T_mb)
             H_hat = supervisor(E_hat, T_mb)
-            H_hat_supervise = supervisor(H, T_mb)
+            H_hat_supervise = supervisor(H_supervise, T_mb) 
             X_hat = recovery(H_hat, T_mb)
 
             G_loss_U = criterion(discriminator(H_hat, T_mb), torch.ones((batch_size,max_seq_len, 1)))
             G_loss_U_e = criterion(discriminator(E_hat, T_mb), torch.ones((batch_size,max_seq_len, 1)))
-            G_loss_S = torch.sqrt(mse_loss(H[:, 1:, :], H_hat_supervise[:, :-1, :]))
+            G_loss_S = torch.sqrt(mse_loss(H_supervise[:, 1:, :], H_hat_supervise[:, :-1, :]))
             G_loss_V1 = torch.mean(torch.abs(torch.sqrt(torch.var(X_hat, dim=0) + 1e-6)
                                               - torch.sqrt(torch.var(ori_data, dim=0) + 1e-6)))
             G_loss_V2 = torch.mean(torch.abs(torch.mean(X_hat, dim=0) - torch.mean(ori_data, dim=0)))
@@ -236,11 +280,11 @@ def timegan(ori_data, parameters):
             # Train embedder
             E_hat = generator(Z_mb, T_mb)
             H_hat = supervisor(E_hat, T_mb)
-            H_hat_supervise = supervisor(H, T_mb)
+            H_hat_supervise = supervisor(H_supervise, T_mb)
             X_hat = recovery(H_hat, T_mb)
 
             E_loss_T0 = mse_loss(X_mb, X_hat)
-            E_loss = 10 * torch.sqrt(E_loss_T0) + 0.1 * torch.sqrt(mse_loss(H[:, 1:, :], H_hat_supervise[:, :-1, :]))
+            E_loss = 10 * torch.sqrt(E_loss_T0) + 0.1 * torch.sqrt(mse_loss(H_supervise[:, 1:, :], H_hat_supervise[:, :-1, :]))
 
             E_loss.backward()
             embedder_optimizer.step()
@@ -258,7 +302,7 @@ def timegan(ori_data, parameters):
         Z_mb = torch.stack([torch.tensor(z) for z in Z_mb])
 
         # Discriminator training
-        D_loss_real = criterion(discriminator(H, T_mb), torch.ones((batch_size,max_seq_len, 1)))
+        D_loss_real = criterion(discriminator(H_supervise, T_mb), torch.ones((batch_size,max_seq_len, 1)))
         D_loss_fake = criterion(discriminator(H_hat.detach(), T_mb), torch.zeros((batch_size,max_seq_len, 1)))
         D_loss_fake_e = criterion(discriminator(E_hat.detach(), T_mb), torch.zeros((batch_size,max_seq_len, 1)))
         D_loss = D_loss_real + D_loss_fake + gamma * D_loss_fake_e
@@ -280,7 +324,8 @@ def timegan(ori_data, parameters):
     # Synthetic data generation
     Z_mb = random_generator(no, z_dim, ori_time, max_seq_len)
     
-    Z_mb = torch.stack([torch.tensor(z) for z in Z_mb])
+    Z_mb = torch.stack([torch.tensor(z, dtype=torch.float32) for z in Z_mb])
+    ori_time = torch.stack([torch.tensor(t, dtype=torch.int64) for t in ori_time])
 
     generated_data_curr = recovery(generator(Z_mb, ori_time), ori_time).detach().numpy()
 
